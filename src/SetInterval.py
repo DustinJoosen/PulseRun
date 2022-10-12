ce195d0d23c445a19d43aa418e08fa32eb2fdc7f
DustinJoosen <dustinjoosen2003@gmail.com>
2022-10-12 09:40:03 +0200

    clean setinterval up

--- src/SetInterval.py
+++ src/SetInterval.py
@@ -6,16 +6,16 @@ class SetInterval:
 	def __init__(self, action, interval):
 		self.action = action
 		self.interval = interval
-		self.stopEvent = threading.Event()
+		self.stop_event = threading.Event()
 
-		thread = threading.Thread(target=self.__setInterval)
+		thread = threading.Thread(target=self.__set_interval)
 		thread.start()
 
-	def __setInterval(self):
-		nextTime = time.time()+self.interval
-		while not self.stopEvent.wait(nextTime-time.time()):
-			nextTime += self.interval
+	def __set_interval(self):
+		next_time = time.time() + self.interval
+		while not self.stop_event.wait(next_time - time.time()):
+			next_time += self.interval
 			self.action()
 
 	def cancel(self):
-		self.stopEvent.set()
+		self.stop_event.set()
